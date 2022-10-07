8c68b3953f2c8ed6436238510b285737d9144ba2
B0272558 <b0272558@LTB0272558-Mac.local>
2022-10-07 12:10:53 +0530

    get apis

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ let colorRepo = require('./repos/colorRepo');
 // Use the express Router object
 let router = express.Router();
 
-// Array of data
-let pies = pieRepo.get();
-
 // Create GET to return a list of all the pies
 // It is a good practice to pass the JSON object
 // Which contains status code and status message
 router.get('/pies', function(req, res, next) {
-    res.status(200).json({
-        "status": 200,
-        "statusText": "OK",
-        "message": "All pies retrieved",
-        "data": pies
+    pieRepo.get(function (data) {
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "All pies retreieved",
+            "data" : data
+        });
+    }, function(err) {
+        next(err);
     });
 });
 
@@ -33,6 +34,28 @@ router.get('/colors', function(req, res, next) {
             "message": "All colors retreieved",
             "data" : data
         });
+    }, function(err) {
+        next(err);
+    });
+});
+
+// Get a single pie using /pies/{pie_id}
+router.get('/pies/:id', function(req, res, next) {
+    pieRepo.getByID(req.params.id, function(data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "Single pie retrieved",
+                "data": data
+            });
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not found",
+                "message": "The pie '" + req.params.id + "' could not be found",
+            });
+        }
     }, function(err) {
         next(err);
     });
--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,11 +1,29 @@
+let fs = require('fs');
+
+const FILE_NAME = './assets/pies.json';
+
 let pieRepo = {
-    get: function() {
-        return [
-            {"id": 1, "name": "Apple"},
-            {"id": 2, "name": "Cherry"},
-            {"id": 3, "name": "Peach"}
-        ];
+    get: function (resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(JSON.parse(data));
+            }
+        })
+    }, 
+    getByID: function (id, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                let pie = JSON.parse(data).find(p => p.id == id)
+                resolve(pie);
+            }
+        })
     }
-};
+}
 
 module.exports = pieRepo;
